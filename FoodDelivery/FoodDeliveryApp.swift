4f2aa7ae38efbc9bb83184aec7ee50fd821493fc
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2020-12-29 11:43:58 -0500

    Initial Commit

---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Hung-Chun Tsai on 2020-12-29.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
